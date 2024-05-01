cef93685ce5eda4b5abd46b907b82204b9967f5f
Miguel Angel Peñafiel Cadena <124106682+mpenafielc5@users.noreply.github.com>
2024-05-01 16:05:43 -0500

    Delete ventas_python directory

--- ventas_python/paths.py
+++
@@ -1,3 +0,0 @@
-import os
-path, _ = os.path.split(os.path.abspath(__file__))
-print("path=",path)
--- ventas_python/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
- # def __str__(self):
-    #     # Método especial para representar la clase Cliente como una cadena
-    #     return f'Cliente: {self.first_name} {self.last_name}'  
